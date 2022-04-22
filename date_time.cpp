c25ec09b67c22278b4f07c5477de9dab11b2b9d8
Jed Pauckner <64628994+Deadjed@users.noreply.github.com>
2022-04-22 11:15:17 +1000

    Update date_time.cpp

--- date_time.cpp
+++ date_time.cpp
@@ -52,24 +52,24 @@ void date_time::get_time(FROM SYSTEM)
 void date_time::print_bits()
 {
 	// print bits
-	std::cout << "\n";									// LINE 1
-	std::cout << std::bitset<4>(month)[0];				// month bit
-	std::cout << 0b0 << 0b0;							// 2 empty bits
-	std::cout << std::bitset<5>(day) << "\n";			// day bits
+	std::cout << "\n";					// LINE 1
+	std::cout << std::bitset<4>(month)[0];			// month bit
+	std::cout << 0b0 << 0b0;				// 2 empty bits
+	std::cout << std::bitset<5>(day) << "\n";		// day bits
 
-														// LINE 2
-	std::cout << std::bitset<4>(month)[1];				// month bit
-	std::cout << 0b0 << 0b0;							// empty bit
-	std::cout << std::bitset<5>(hour) << "\n";			// hour bits
+								// LINE 2
+	std::cout << std::bitset<4>(month)[1];			// month bit
+	std::cout << 0b0 << 0b0;				// empty bit
+	std::cout << std::bitset<5>(hour) << "\n";		// hour bits
 
-														// LINE 3
-	std::cout << std::bitset<4>(month)[2];				// month bit
-	std::cout << 0b0;									// empty bit
+								// LINE 3
+	std::cout << std::bitset<4>(month)[2];			// month bit
+	std::cout << 0b0;					// empty bit
 	std::cout << std::bitset<6>(minute) << "\n";		// minute bits
 
-														// LINE 4
-	std::cout << std::bitset<4>(month)[3];				// month bit
-	std::cout << 0b0;									// empty bit
+								// LINE 4
+	std::cout << std::bitset<4>(month)[3];			// month bit
+	std::cout << 0b0;					// empty bit
 	std::cout << std::bitset<6>(second) << std::endl;	// second bits
 }
 
@@ -132,20 +132,3 @@ void date_time::check_date()
 }
 
 #endif // !DATE_TIME_H
-
-/*
- * 1  0001 = 31
- * 3  0011 = 31
- * 5  0101 = 31
- * 7  0111 = 31
- * 8  1000 = 31
- * 10 1010 = 31
- * 12 1100 = 31
-
- * 2  0010 = 28
-
- * 4  0100 = 30
- * 6  0110 = 30
- * 9  1001 = 30
- * 11 1011 = 30
- */
